670889d4776ea8d09fb7912c31600d5aa44d084d
rivydhoassidiq <blumeteam01@google.com>
2022-05-19 22:32:33 +0800

    commit first

---
+++ android/app/src/main/kotlin/com/example/dhio_punya/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dhio_punya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:dhio_punya/title_model.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: TfitleModel(),
+    );
+  }
+}
